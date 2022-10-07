ee3eb395127c17a262d5bca3163caeaccd46d8b5
Jake Staehle <jstaehle@roku.com>
2022-10-07 14:56:08 -0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example Autotools Application
+
+This one uses the example Autotool's lib and CMake lib as dependencies to create: `buzz_aldrin`
+
---
+++ src/buzz.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <jupiter.h>
+
+int main(int argc, char const *argv[]) {
+    std::cout << europa_name() << '\n';
+    std::cout << europa_new_friend() << '\n';
+}
+
